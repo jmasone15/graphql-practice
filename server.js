7c7257d3e850fdc4dfa958cd555b723688aa0ba5
Jordan Masone <jordanmasone1@gmail.com>
2022-06-06 20:52:40 -0400

    MongoDB and mongoose setup.

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Connect to database
+connectDB();
+
 app.use("/graphql", graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "development"
-}))
+}));
+
+app.use(morgan("tiny"));
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`.magenta.bold.underline);
 });
