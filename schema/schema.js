3ee6379dfe241b71d284da2ba486f88b669adb4e
Jordan Masone <jordanmasone1@gmail.com>
2022-06-06 21:12:50 -0400

    Added mutation for addClient.

--- schema/schema.js
+++ schema/schema.js
@@ -1,11 +1,13 @@
-const { projects, clients } = require("../sampleData");
+const Project = require("../models/Project");
+const Client = require("../models/Client");
 
 const { 
     GraphQLObjectType, 
     GraphQLID, 
     GraphQLString, 
     GraphQLSchema, 
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = require("graphql");
 
 // Project Type
@@ -19,7 +21,7 @@ const ProjectType = new GraphQLObjectType({
         client: {
             type: ClientType,
             resolve(parent, args) {
-                return clients.find(client => client.id === parent.clientId);
+                return Client.findById(parent.id);
             }
         }
     })
@@ -36,6 +38,7 @@ const ClientType = new GraphQLObjectType({
     })
 });
 
+// Queries
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     fields: {
@@ -43,26 +46,50 @@ const RootQuery = new GraphQLObjectType({
             type: ProjectType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return projects.find(project => project.id === args.id);
+                return Project.findById(args.id);
             }
         },
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args) {
-                return projects;
+                return Project.find();
             }
         },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id);
+                return Client.findById(args.id);
             }
         },
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
-                return clients;
+                return Client.find();
+            }
+        }
+    }
+});
+
+// Mutations
+const mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addClient: {
+            type: ClientType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                email: { type: GraphQLNonNull(GraphQLString) },
+                phone: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parent, args) {
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone
+                });
+
+                return client.save();
             }
         }
     }
@@ -70,5 +97,6 @@ const RootQuery = new GraphQLObjectType({
 
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 });
