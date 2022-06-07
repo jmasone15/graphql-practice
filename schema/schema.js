e43fcb1763429272da0136a143776e47c0a189b2
Jordan Masone <jordanmasone1@gmail.com>
2022-06-06 21:16:26 -0400

    Added delete mutation for clients.

--- schema/schema.js
+++ schema/schema.js
@@ -75,6 +75,7 @@ const RootQuery = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
     name: "Mutation",
     fields: {
+        // Add Client
         addClient: {
             type: ClientType,
             args: {
@@ -90,6 +91,16 @@ const mutation = new GraphQLObjectType({
                 });
 
                 return client.save();
+            }
+        },
+        // Delete Client
+        deleteClient: {
+            type: ClientType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLID) },
+            },
+            resolve(parent, args) {
+                return Client.findByIdAndRemove(args.id)
             }
         }
     }
